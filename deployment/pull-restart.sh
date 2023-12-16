c72649978c93006d20a255d093f432b925cbb141
Sam <49539910+KingShampoo@users.noreply.github.com>
2023-12-15 20:29:36 -0500

    fixing deployment

--- deployment/pull-restart.sh
+++ deployment/pull-restart.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo docker kill 
-sudo docker rm 
-sudo docker image rm 
-sudo docker pull 
-sudo docker run -d -p 80:80 --name 
+sudo docker kill website
+sudo docker rm website
+sudo docker image rm koolnoob/f23cicd-kingshampoo:latest
+sudo docker pull koolnoob/f23cicd-kingshampoo:latest
+sudo docker run -d -p 80:80 --name  website koolnoob/f23cicd-kingshampoo:latest
